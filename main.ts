0734bbb0ee813e4c45341ac62a854147751490b1
Mike <miky4321@live.it>
2022-04-21 00:11:18 +0200

    some kind of beginning

--- main.ts
+++ main.ts
@@ -1,42 +1,31 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { BrowserWindow } from 'electron';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Braincache extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+        this.addRibbonIcon('sync', 'braincache', (evt: MouseEvent) => {
+            const token = globalThis.localStorage.getItem('braincache-token')
+            if(!token){
+                new LoginModal(this.app).open()
+            } else {
+                new Notice('syncing decks...');
+            }
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const cardCountEl = this.addStatusBarItem();
+		cardCountEl.setText('0 cards');
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-login-modal',
+			name: 'login',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new LoginModal(this.app).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
+    /* editor callback 
+    
 		this.addCommand({
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
@@ -44,8 +33,12 @@ export default class MyPlugin extends Plugin {
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		}); 
+
+    */
+
+		/* check app status before callback
+   
 		this.addCommand({
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
@@ -56,7 +49,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new LoginModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -65,52 +58,127 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
+    */
+
+		/* add settings
+     
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
+    async saveSettings() {
+      await this.saveData(this.settings);
+    }
+
+    */
+
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+    // cleanup
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
 	}
 
 	onunload() {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
+type loginArgs = (email: string, password: string) => Promise<{status: boolean, token?: string}>
+
+const login: loginArgs = async(email, password) => {
+    const res = await fetch('https://api.braincache.co/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            })
+    const json = await res.json()
+    console.log(json)
+
+    return {
+        status: true,
+        token: 'testing'
+    }
+}
+
+class LoginModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { titleEl, contentEl } = this;
+        const title = titleEl.createDiv('login-title')
+		title.createEl('img', { attr: 
+            {src: 'https://braincache.co/android-chrome-192x192.baef1f58.png'}
+        })
+        title.createEl('p', { text: 'login'})
+		/*contentEl.createEl('p', { text: 'please login with your braincache credentials to continue' });
+        const form = contentEl.createDiv('login-form')
+        form.createEl('input', { attr: { type: 'text', placeholder: 'email' }})
+        form.createEl('input', { attr: { type: 'password', placeholder: 'password'}})
+
+        const buttonLogin = form.createEl('button', { text: 'login'})*/
+
+        // create an iframe and go on github.com
+        const iframe = contentEl.createEl('iframe', { attr: { src: 'https://braincache.co/community' }, cls: 'login-iframe'})
+        console.log(iframe)
+
+
+/*        buttonLogin.onclick = async() => {
+            const elements = form.getElementsByTagName('input')
+            const email = elements[0].value
+            const password = elements[1].value
+            /*const trial = await login(email, password)
+
+            if(trial.status){
+                new Notice('login successful')
+            } else {
+                new Notice('login failed')
+            }
+
+            fetch('https://braincache.co/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            })
+            .then(res => res.json())
+            .then(res => {
+                if(res.success){
+                    globalThis.localStorage.setItem('braincache-token', res.token)
+                    this.close()
+                } else {
+                    new Notice('invalid credentials')
+                }
+            })
+            .catch(err => {
+                new Notice('error logging in')
+            })*/
+        
+
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
+
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class BraincacheSettingTab extends PluginSettingTab {
+	plugin: Braincache;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Braincache) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
