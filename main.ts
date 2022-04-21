cd6f19f4403d3774d83433d5ae6e6c1e264cbc85
Mike <miky4321@live.it>
2022-04-21 17:38:13 +0200

    token based auth flow

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { BrowserWindow } from 'electron';
+import * as electron from 'electron';
 
 export default class Braincache extends Plugin {
 
 	async onload() {
         this.addRibbonIcon('sync', 'braincache', (evt: MouseEvent) => {
+			electron.remote.app.commandLine.appendSwitch('allow-insecure-localhost', 'true')
             const token = globalThis.localStorage.getItem('braincache-token')
             if(!token){
                 new LoginModal(this.app).open()
@@ -20,22 +21,23 @@ export default class Braincache extends Plugin {
 			id: 'open-login-modal',
 			name: 'login',
 			callback: () => {
-				new LoginModal(this.app).open();
+				console.log("login")
+				//new LoginModal(this.app).open();
 			}
 		});
 
-    /* editor callback 
-    
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		}); 
+		/* editor callback 
+		
+			this.addCommand({
+				id: 'sample-editor-command',
+				name: 'Sample editor command',
+				editorCallback: (editor: Editor, view: MarkdownView) => {
+					console.log(editor.getSelection());
+					editor.replaceSelection('Sample Editor Command');
+				}
+			}); 
 
-    */
+		*/
 
 		/* check app status before callback
    
@@ -114,58 +116,22 @@ class LoginModal extends Modal {
 	onOpen() {
 		const { titleEl, contentEl } = this;
         const title = titleEl.createDiv('login-title')
+
 		title.createEl('img', { attr: 
             {src: 'https://braincache.co/android-chrome-192x192.baef1f58.png'}
         })
-        title.createEl('p', { text: 'login'})
-		/*contentEl.createEl('p', { text: 'please login with your braincache credentials to continue' });
+
+		title.createEl('p', { text: 'login'})
+		contentEl.createEl('p', { text: 'paste your braincache token to continue' });
+
         const form = contentEl.createDiv('login-form')
-        form.createEl('input', { attr: { type: 'text', placeholder: 'email' }})
-        form.createEl('input', { attr: { type: 'password', placeholder: 'password'}})
+        const input = form.createEl('input', { attr: { type: 'password', placeholder: 'obsidian-token' }})
+        const buttonLogin = form.createEl('button', { text: 'save'})
 
-        const buttonLogin = form.createEl('button', { text: 'login'})*/
-
-        // create an iframe and go on github.com
-        const iframe = contentEl.createEl('iframe', { attr: { src: 'https://braincache.co/community' }, cls: 'login-iframe'})
-        console.log(iframe)
-
-
-/*        buttonLogin.onclick = async() => {
-            const elements = form.getElementsByTagName('input')
-            const email = elements[0].value
-            const password = elements[1].value
-            /*const trial = await login(email, password)
-
-            if(trial.status){
-                new Notice('login successful')
-            } else {
-                new Notice('login failed')
-            }
-
-            fetch('https://braincache.co/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            })
-            .then(res => res.json())
-            .then(res => {
-                if(res.success){
-                    globalThis.localStorage.setItem('braincache-token', res.token)
-                    this.close()
-                } else {
-                    new Notice('invalid credentials')
-                }
-            })
-            .catch(err => {
-                new Notice('error logging in')
-            })*/
-        
-
+        buttonLogin.onclick = async() => {
+            globalThis.localStorage.setItem('braincache-token', input.value)
+			this.close()
+		}
 	}
 
 	onClose() {
