92232fe6bbd346ffeb38894e2c5063b299a1e5e8
Mike <miky4321@live.it>
2022-04-24 02:57:03 +0200

    removed boilerplate

--- main.ts
+++ main.ts
@@ -1,117 +1,45 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Modal, Notice, Plugin } from 'obsidian';
+
+const token = () => globalThis.localStorage.getItem('braincache-token');
 
 export default class Braincache extends Plugin {
 
 	async onload() {
-        this.addRibbonIcon('sync', 'braincache', (evt: MouseEvent) => {
-            const token = globalThis.localStorage.getItem('braincache-token')
-            document.cookie = `session=${token}`
+        this.addRibbonIcon('sync', 'braincache', (_ : MouseEvent) => {
+            document.cookie = `session=${token()}`
             if(!token){
-                new LoginModal(this.app).open()
+              new LoginModal(this.app).open()
             } else {
-              fetch("https://api.braincache.co/auth/status", {
-                headers: {
-                  cookie: `session=${token}`
-                },
-                credentials: 'include'
-              }).then((res) => {
-                console.log(res)
-              })
+              this.syncDecks()
             }
 		});
 
 		const cardCountEl = this.addStatusBarItem();
 		cardCountEl.setText('0 cards');
 
-		this.addCommand({
-			id: 'open-login-modal',
-			name: 'login',
-			callback: () => {
-				console.log("login")
-				//new LoginModal(this.app).open();
-			}
-		});
+	}
 
-		/* editor callback 
-		
-			this.addCommand({
-				id: 'sample-editor-command',
-				name: 'Sample editor command',
-				editorCallback: (editor: Editor, view: MarkdownView) => {
-					console.log(editor.getSelection());
-					editor.replaceSelection('Sample Editor Command');
-				}
-			}); 
+  async syncDecks() {
+    new Notice("syncing braincache decks...")
 
-		*/
+    const res = await fetch("https://api.braincache.co/auth/status", {
+      headers: {
+        cookie: `session=${token()}`
+      },
+      credentials: 'include'
+    })
 
-		/* check app status before callback
-   
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new LoginModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-    */
-
-		/* add settings
-     
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-    async saveSettings() {
-      await this.saveData(this.settings);
+    if(res.status !== 200){
+      localStorage.removeItem('braincache-token');
+      new Notice("failed to sync decks, login again")
     }
 
-    */
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		});
-
-    // cleanup
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
-	}
+  }
 
 	onunload() {
 
 	}
 
-}
-
-type loginArgs = (email: string, password: string) => Promise<{status: boolean, token?: string}>
-
-const login: loginArgs = async(email, password) => {
-    const res = await fetch('https://api.braincache.co/auth/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            })
-    const json = await res.json()
-    console.log(json)
-
-    return {
-        status: true,
-        token: 'testing'
-    }
 }
 
 class LoginModal extends Modal {
@@ -145,33 +73,4 @@ class LoginModal extends Modal {
 		contentEl.empty();
 	}
 
-}
-
-class BraincacheSettingTab extends PluginSettingTab {
-	plugin: Braincache;
-
-	constructor(app: App, plugin: Braincache) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
