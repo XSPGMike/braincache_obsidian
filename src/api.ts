76e625387cab9ebe301e8d1fdc9100052d110f80
Mike <miky4321@live.it>
2022-04-26 17:30:48 +0200

    checking deck status

--- src/api.ts
+++ src/api.ts
@@ -22,7 +22,42 @@ export async function checkAuth(): Promise<boolean> {
   return false
 }
 
-async function createDeck(deckName: string): Promise<string>{
+async function remoteDeckExists(deckId: string): Promise<boolean> {
+  let res = await fetch(`https://api.braincache.co/decks/${deckId}`, {
+    headers: {
+    "Authorization": `Bearer ${token()}`
+    }
+  })
+
+  return res.status === 200
+}
+
+async function findRemoteDeck(deckName: string): Promise<string> {
+  let res = await fetch(`https://api.braincache.co/decks`, {
+    headers: {
+      "Authorization": `Bearer ${token()}`
+    }
+  })
+  const decks = await res.json()
+  return decks.find((d: any) => d.name === deckName)?.deckId
+}
+
+async function findOrCreateDeck(deckName: string): Promise<string>{
+  let deckId = localStorage.getItem(`bcDeck_${deckName}`)
+
+  // check if deck is still present on the server
+  if(deckId){
+    if(await remoteDeckExists(deckId)){
+      return deckId
+    }
+  }
+
+  deckId = await findRemoteDeck(deckName)
+
+  if(deckId){
+    return deckId
+  }
+
   const response = await fetch("https://api.braincache.co/decks/", {
     method: "POST",
     headers: {
@@ -41,7 +76,8 @@ async function createDeck(deckName: string): Promise<string>{
 export async function uploadCards(cardSets: BcSet[]): Promise<{status: boolean}>{
   const promises = []
   for(const set of cardSets){
-    const deckId = await createDeck(set.deckName);
+    const deckId = await findOrCreateDeck(set.deckName);
+    localStorage.setItem(`bcDeck_${set.deckName}`, deckId)
     for(const card of set.cards){
       promises.push(fetch(`https://api.braincache.co/decks/${deckId}/card`, {
         method: "POST",
